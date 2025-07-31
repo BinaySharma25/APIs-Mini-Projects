b1d2cfcde37a11477213fee2f978ca7c30c75a1b
Binay Kumar <165971861+BinaySharma25@users.noreply.github.com>
2025-07-31 21:53:32 +0530

    Update Api.js

--- Cat-Facts/Api.js
+++ Cat-Facts/Api.js
@@ -7,12 +7,12 @@ async function getFacts() {
     try{
         let res = await fetch(url);   
         let data = await res.json();
-        console.log(data.fact);
+       // console.log(data.fact);
         fact1.innerText = data.fact;
 
         let res2 = await fetch(url);
         let data2 = await res2.json();
-        console.log(data2.fact);
+       // console.log(data2.fact);
         fact2.innerText = data2.fact;
         
 
